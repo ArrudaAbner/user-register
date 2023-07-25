b5848691ff56910a3fc70846937689e0c38375a7
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-24 22:39:07 -0300

    frontend
    logo component

--- front-end/src/components/template/Logo.jsx
+++ front-end/src/components/template/Logo.jsx
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-anonymous-default-export */
-import "./Logo.css"
-import React from "react"
+import "./Logo.css";
+import React from "react";
+import logo from "../../assets/imgs/logo.png"
 
-export default props => {
-    return(
-        <aside className="logo">
-            Logo
-        </aside>
-    )
-}
+export default (props) => {
+  return (
+    <aside className="logo">
+      <a href="/" className="logo">
+        <img src={logo} alt="logo" />
+      </a>
+    </aside>
+  );
+};
