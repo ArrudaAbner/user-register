e80e149649845bf3420ce55968a9490ab734dd6a
Abner Rodrigues de Arruda <62723779+ArrudaAbner@users.noreply.github.com>
2023-07-24 23:13:09 -0300

    frontend
    nav component

--- front-end/src/components/template/Nav.jsx
+++ front-end/src/components/template/Nav.jsx
@@ -4,6 +4,16 @@ import React from "react";
 
 export default (props) => {
   return (
-     <aside className="menu-area">Navegação</aside>
+    <aside className="menu-area">
+      <nav className="menu">
+        {/* Refatorar para componentes isolados */}
+        <a href="#/">
+          <i className="fa fa-home"></i> Início
+        </a>
+        <a href="#/users">
+          <i className="fa fa-users"></i> Usuários
+        </a>
+      </nav>
+    </aside>
   );
 };
